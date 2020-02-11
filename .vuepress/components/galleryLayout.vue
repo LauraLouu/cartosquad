29666063d9b65bc6a38c92fa82f5abfd9e948a7e
Laura <le@ktgis.de>
2020-02-11 15:07:22 +0100

    Update galleryLayout.vue

--- .vuepress/components/galleryLayout.vue
+++ .vuepress/components/galleryLayout.vue
@@ -1,12 +1,11 @@
 <template>
   <div class="post-container" v-for="page in pages">
-  
       <div class="post-card">
         <img class="article-image" src="{{ page.frontmatter.map-link }}" />
         <div class="page-detail">
           <div class="page-title">{{ page.title }}</div>
           <div class="page-description">{{ page.frontmatter.description }}</div>
-          <div class="page-author">Author: <a href="{{ page.frontmatter.source-link }}" target="_blank">{{ page.frontmatter.author }}</a></div>
+          <div class="page-author">Author: <authorLink :to="page.frontmatter.source-link" target="_blank">{{ page.frontmatter.author }}</authorLink></div>
           <div class="read-more"><router-link :to="page.path">read more →</router-link></div> 
         </div>
       </div>
