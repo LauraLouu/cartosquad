fcfa26a98314cabf3325ae0300490c5f8d067dcb
Laura <le@ktgis.de>
2020-02-11 15:11:47 +0100

    Update galleryLayout.vue

--- .vuepress/components/galleryLayout.vue
+++ .vuepress/components/galleryLayout.vue
@@ -5,7 +5,7 @@
         <div class="page-detail">
           <div class="page-title">{{ page.title }}</div>
           <div class="page-description">{{ page.frontmatter.description }}</div>
-          <div class="page-author">Author: <authorLink :to="page.frontmatter.source-link" target="_blank">{{ page.frontmatter.author }}</authorLink></div>
+          <div class="page-author">Author: <authorLink :to="{{ page.frontmatter.source-link }}" target="_blank">{{ page.frontmatter.author }}</authorLink></div>
           <div class="read-more"><router-link :to="page.path">read more →</router-link></div> 
         </div>
       </div>
@@ -38,7 +38,7 @@ export default {
   width: 600px;
   height: 150px;
   margin: 10px;
-  border: 2px solid $accentColor;
+  border: 2px solid #247ba0;
   border-radius: 3px;
   padding: 10px;
   display: flex;
