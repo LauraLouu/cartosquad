f88ffb02174c00ab9a8923f30872b68e11bc29f3
Laura <le@ktgis.de>
2020-02-12 09:43:48 +0100

    Update galleryLayout.vue

--- .vuepress/components/galleryLayout.vue
+++ .vuepress/components/galleryLayout.vue
@@ -5,11 +5,11 @@
         <div class="page-detail">
           <div class="page-title">{{ page.title }}</div>
           <div class="page-categroy"><span class="list">Category: </span>{{ page.frontmatter.category }}</div>
-          <div class="page-author"><span class="list">Author: </span><a v-bind:href="page.frontmatter.authorLink">{{ page.frontmatter.author }}</a></div>
-          <div class="page-location"><span class="list">Location: </span><a v-bind:href="page.frontmatter.locationLink">{{ page.frontmatter.location }}</a></div>
+          <div class="page-author"><span class="list">Author: </span><a target="_blank" v-bind:href="page.frontmatter.authorLink">{{ page.frontmatter.author }}</a></div>
+          <div class="page-location"><span class="list">Location: </span><a target="_blank" v-bind:href="page.frontmatter.locationLink">{{ page.frontmatter.location }}</a></div>
           <div class="page-description">{{ page.frontmatter.description }}</div>
           <!--div class="read-more"><router-link :to="page.path">read more →</router-link></div--> 
-          <div class="read-more"><a v-bind:href="page.frontmatter.sourceLink">see original →</a></div> 
+          <div class="read-more"><a target="_blank" v-bind:href="page.frontmatter.sourceLink">see original →</a></div> 
         </div>
       </div>
       
@@ -59,7 +59,7 @@ export default {
     font-size: 1.25em;
 }
 .post-description {
-  padding-top:10px;
+  padding:10px 0;
 }
 .list {
   font-weight:550;
